ceeecb1444fa979120f9b9451d274120ff39e345
madandan <madandan@gmail.com>
2017-07-11 16:45:06 +0800

    Initial commit

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by madandan on 2017/7/11.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
